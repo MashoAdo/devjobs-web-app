12a21ebf47b1ec2575b5f0df3a8aab97c1e354ac
Masho Ado <mashoado@gmail.com>
2022-03-17 23:06:59 +0300

    find job with id specified from request url path and send it back as a response

---
+++ controllers/individualJobController.js
@@ -0,0 +1,18 @@
+const DevJob = require("../models/Job");
+
+// find job with id specified from request url path and send it back as a response
+
+const getIndividualJob = async (req, res) => {
+	const id = req.params["id"];
+	try {
+		DevJob.findById(id, (err, job) => {
+			if (err) throw "fetching from db failed";
+
+			res.send(job);
+		});
+	} catch (error) {
+		res.send(error);
+	}
+};
+
+module.exports = { getIndividualJob };
