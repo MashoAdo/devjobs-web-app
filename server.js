b688ea8d6472ff99c731de51edee41ca3fc6719d
Masho Ado <mashoado@gmail.com>
2022-03-22 22:35:50 +0300

    add middleware to handle static files, add endpoint that serves build files when root route is hit

--- server.js
+++ server.js
@@ -16,12 +16,15 @@ connectDB();
 // middlewares
 server.use(cors());
 server.use(express.json());
+server.use(express.static(path.join(__dirname, "client", "build")));
 
 // routes
 server.use("/jobs", require("./routes/jobs"));
 server.use("/", require("./routes/individualJob"));
+
+// server build(contains React UI)
 server.get("/", (req, res) => {
-	res.send("home page");
+	res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
 
 // listens to server once connection to database has been established
